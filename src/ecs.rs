d993916fb1c50a997201d55acc429d53b6046d91
Peter Mandile <peterm1994@hotmail.com>
2022-05-31 08:43:19 +1000

    Fixed metadata

--- src/ecs.rs
+++ src/ecs.rs
@@ -5,6 +5,7 @@ use wasm_bindgen::prelude::*;
 use web_sys::console;
 
 type EcsId = usize;
+type CompType = String;
 
 
 #[derive(Default)]
@@ -12,11 +13,22 @@ pub struct Components {
     components: Vec<Box<RefCell<dyn std::any::Any>>>,
 }
 
+struct Entity {
+    entity_id: EcsId,
+    components: HashMap<CompType, Box<RefCell<dyn std::any::Any>>>,
+}
+
+impl Entity {
+    fn new(entity_id: EcsId) -> Self {
+        Self { entity_id, components: Default::default() }
+    }
+}
+
 
 #[wasm_bindgen]
 #[derive(Default)]
 pub struct Game {
-    entities: HashMap<EcsId, Components>,
+    entities: HashMap<EcsId, Entity>,
     ecs_id_count: EcsId,
 }
 
@@ -33,13 +45,13 @@ impl Game {
     pub fn add_entity(&mut self) -> EcsId {
         let entity_id = self.ecs_id_count;
         self.ecs_id_count += 1;
-        self.entities.insert(entity_id, Components::default());
+        self.entities.insert(entity_id, Entity::new(entity_id));
 
         return entity_id;
     }
 
     pub fn add_component_web(&mut self, entity_id: EcsId, component: JsValue) -> String {
-        console::log_1(&format!("hello").into());
+        console::log_1(&format!("Adding web component").into());
 
         let proto = js_sys::Object::get_prototype_of(&component);
         let constructor = proto.constructor();
@@ -48,3 +60,20 @@ impl Game {
         return name;
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::ecs::Game;
+
+    #[test]
+    fn test_native_comp() {
+        struct A;
+        struct B;
+
+        let mut game = Game::new("");
+
+        game.
+    }
+}
+
+struct Comp;
